467a7911d0b956f03be5ca53dafecfb9142806c8
jens <jensdenijs@hotmail.com>
2021-08-03 15:03:28 +0200

    functional loggers in MonologgerController.php

--- src/Controller/MonologgerController.php
+++ src/Controller/MonologgerController.php
@@ -14,18 +14,86 @@ use Symfony\Config\MonologConfig;
 class MonologgerController extends AbstractController
 {
 
-    /**
-     * @Route("/monologger", name="monologger")
-     */
 
+    #[Route('/monologger', name: 'monologger', methods: ['GET', 'POST'])]
     public function index(Request $request): Response
     {
-        $message = $request->query->get("message");
-        $type= $request->query->get("type");
 
-        var_dump($message);
+
+
+        /*    $message = $request->query->get("message");
+            $type = $request->query->get("type");
+
+            var_dump($message);
+            var_dump($type);
+
+
+            $log = new Logger('name');
+            $log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+            $log->warning($message);
+            $log->error('Bar');*/
+
+        return $this->render('monologger/index.html.twig', [
+            'controller_name' => 'MonologgerController',
+        ]);
+    }
+
+    #[Route('/monologger/info', name: 'info', methods: ['POST'])]
+    public function info(Request $request):Response
+    {
+        $message = $request->get("message");
+        $type = $request->get("type");
+        
+        $log = new Logger('info');
+        $log->pushHandler(new StreamHandler('../src/logs/info.log', Logger::INFO));
+        $log->info($type.': '.$message);
+
+        return $this->render('monologger/index.html.twig', [
+            'controller_name' => 'MonologgerController',
+        ]);
+    }
+
+    #[Route('/monologger/error', name: 'error', methods: ['POST'])]
+    public function error(Request $request):Response
+    {
+        $message = $request->get("message");
+        $type = $request->get("type");
+
+        $log = new Logger('error');
+        $log->pushHandler(new StreamHandler('../src/logs/warning.log', Logger::WARNING));
+        $log->warning($type.': '.$message);
+
+        return $this->render('monologger/index.html.twig', [
+            'controller_name' => 'MonologgerController',
+        ]);
+    }
+
+    #[Route('/monologger/warning', name: 'warning', methods: [ 'POST'])]
+    public function warning(Request $request):Response
+    {
+        $message = $request->get("message");
+        $type = $request->get("type");
+
         var_dump($type);
+        $log = new Logger('warning');
+        $log->pushHandler(new StreamHandler('../src/logs/warning.log', Logger::WARNING));
+        $log->warning($type.': '.$message);
 
+        return $this->render('monologger/index.html.twig', [
+            'controller_name' => 'MonologgerController',
+        ]);
+    }
+
+    #[Route('/monologger/emergency', name: 'emergency', methods: [ 'POST'])]
+    public function emergency(Request $request):Response
+    {
+        $message = $request->get("message");
+        $type = $request->get("type");
+
+        $log = new Logger('emergency');
+        $log->pushHandler(new StreamHandler('../src/logs/emergency.log', Logger::EMERGENCY));
+        $log->emergency($type.': '.$message);
 
         return $this->render('monologger/index.html.twig', [
             'controller_name' => 'MonologgerController',
